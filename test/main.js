a96a72ea21ea3c33c3391da58b11ee465f324007
Gromych <gromych@yandex-team.ru>
2016-03-03 22:36:21 +0300

    Groups options test and removes file existence check

--- test/main.js
+++ test/main.js
@@ -215,50 +215,47 @@ describe('gulp-replace', function() {
       });
     });
 
-    it('should ignore binary files when skipBinary is enabled', function(done) {
-      var file = new File({
-        path: 'test/fixtures/binary.png',
-        cwd: 'test/',
-        base: 'test/fixtures',
-        contents: fs.readFileSync('test/fixtures/binary.png')
+    describe('options', function () {
+      describe('skipBinary', function () {
+        it('should ignore binary files when skipBinary is enabled', function(done) {
+          var file = new File({
+            path: 'test/fixtures/binary.png',
+            cwd: 'test/',
+            base: 'test/fixtures',
+            contents: fs.readFileSync('test/fixtures/binary.png')
+          });
+
+          var stream = replacePlugin('world', 'person', {skipBinary: true});
+          stream.on('data', function(newFile) {
+            newFile.contents.should.eql(fs.readFileSync('test/expected/binary.png'));
+            done();
+          });
+
+          stream.write(file);
+          stream.end();
+        });
+
+        it('should replace string on non binary files when skipBinary is enabled', function(done) {
+          var file = new File({
+            path: 'test/fixtures/helloworld.txt',
+            cwd: 'test/',
+            base: 'test/fixtures',
+            contents: fs.createReadStream('test/fixtures/helloworld.txt')
+          });
+
+          var stream = replacePlugin('world', 'person', {skipBinary: true});
+          stream.on('data', function(newFile) {
+            newFile.contents.pipe(concatStream({encoding: 'string'}, function(data) {
+              data.should.equal(fs.readFileSync('test/expected/helloworld.txt', 'utf8'));
+              done();
+            }));
+          });
+
+          stream.write(file);
+          stream.end();
+        });
+
       });
-
-      var stream = replacePlugin('world', 'person', {skipBinary: true});
-      stream.on('data', function(newFile) {
-        should.exist(newFile);
-        should.exist(newFile.contents);
-
-        newFile.contents.should.eql(fs.readFileSync('test/expected/binary.png'));
-        done();
-      });
-
-      stream.write(file);
-      stream.end();
     });
-
-    it('should replace string on non binary files when skipBinary is enabled', function(done) {
-      var file = new File({
-        path: 'test/fixtures/helloworld.txt',
-        cwd: 'test/',
-        base: 'test/fixtures',
-        contents: fs.createReadStream('test/fixtures/helloworld.txt')
-      });
-
-      var stream = replacePlugin('world', 'person', {skipBinary: true});
-      stream.on('data', function(newFile) {
-        should.exist(newFile);
-        should.exist(newFile.contents);
-
-        newFile.contents.pipe(concatStream({encoding: 'string'}, function(data) {
-          data.should.equal(fs.readFileSync('test/expected/helloworld.txt', 'utf8'));
-          done();
-        }));
-      });
-
-      stream.write(file);
-      stream.end();
-    });
-
-
   });
 });
