3f8c0a2bb6a431292bcaba2a0b2fa56f7655ab10
Gromych <gromych@yandex-team.ru>
2016-03-03 22:21:19 +0300

    Removes copy-paste of creating file in buffered input mode

--- test/main.js
+++ test/main.js
@@ -8,6 +8,128 @@ var File = require('vinyl');
 
 describe('gulp-replace', function() {
   describe('replacePlugin()', function() {
+    describe('buffered input', function () {
+      var file;
+
+      beforeEach(function () {
+        file = new File({
+          path: 'test/fixtures/helloworld.txt',
+          cwd: 'test/',
+          base: 'test/fixtures',
+          contents: fs.readFileSync('test/fixtures/helloworld.txt')
+        });
+      });
+
+      it('should replace string on a buffer', function(done) {
+        var stream = replacePlugin('world', 'person');
+        stream.on('data', function(newFile) {
+          should.exist(newFile);
+          should.exist(newFile.contents);
+
+          String(newFile.contents).should.equal(fs.readFileSync('test/expected/helloworld.txt', 'utf8'));
+          done();
+        });
+
+        stream.write(file);
+        stream.end();
+      });
+
+      it('should replace regex on a buffer', function(done) {
+        var stream = replacePlugin(/world/g, 'person');
+        stream.on('data', function(newFile) {
+          should.exist(newFile);
+          should.exist(newFile.contents);
+
+          String(newFile.contents).should.equal(fs.readFileSync('test/expected/helloworld.txt', 'utf8'));
+          done();
+        });
+
+        stream.write(file);
+        stream.end();
+      });
+
+      it('should replace regex on a buffer with a function', function(done) {
+        var stream = replacePlugin(/world/g, function() { return 'person'; });
+        stream.on('data', function(newFile) {
+          should.exist(newFile);
+          should.exist(newFile.contents);
+
+          String(newFile.contents).should.equal(fs.readFileSync('test/expected/helloworld.txt', 'utf8'));
+          done();
+        });
+
+        stream.write(file);
+        stream.end();
+      });
+
+      it('should replace string on a buffer with a function', function(done) {
+        var stream = replacePlugin('world', function() { return 'person'; });
+        stream.on('data', function(newFile) {
+          should.exist(newFile);
+          should.exist(newFile.contents);
+
+          String(newFile.contents).should.equal(fs.readFileSync('test/expected/helloworld.txt', 'utf8'));
+          done();
+        });
+
+        stream.write(file);
+        stream.end();
+      });
+
+
+      it('should call function once for each replacement when replacing a string on a buffer', function(done) {
+        var replacements = [
+          'cow',
+          'chicken',
+          'duck',
+          'person'
+        ];
+        var stream = replacePlugin('world', function() { return replacements.shift(); });
+        stream.on('data', function(newFile) {
+          should.exist(newFile);
+          should.exist(newFile.contents);
+
+          String(newFile.contents).should.equal(fs.readFileSync('test/expected/hellofarm.txt', 'utf8'));
+          done();
+        });
+
+        stream.write(file);
+        stream.end();
+      });
+
+
+      it('should call function once for each replacement when replacing a regex on a buffer', function(done) {
+        var replacements = [
+          'cow',
+          'chicken',
+          'duck',
+          'person'
+        ];
+        var stream = replacePlugin(/world/g, function() { return replacements.shift(); });
+        stream.on('data', function(newFile) {
+          should.exist(newFile);
+          should.exist(newFile.contents);
+
+          String(newFile.contents).should.equal(fs.readFileSync('test/expected/hellofarm.txt', 'utf8'));
+          done();
+        });
+
+        stream.write(file);
+        stream.end();
+      });
+
+      it('should trigger events on a buffer', function(done) {
+        var stream = replacePlugin('world', 'elephant')
+        .on('finish', function() {
+          // No assertion required, we should end up here, if we don't the test will time out
+          done();
+        });
+
+        stream.write(file);
+        stream.end();
+      });
+    });
+
     it('should replace string on a stream', function(done) {
       var file = new File({
         path: 'test/fixtures/helloworld.txt',
@@ -25,27 +147,6 @@ describe('gulp-replace', function() {
           data.should.equal(fs.readFileSync('test/expected/helloworld.txt', 'utf8'));
           done();
         }));
-      });
-
-      stream.write(file);
-      stream.end();
-    });
-
-    it('should replace string on a buffer', function(done) {
-      var file = new File({
-        path: 'test/fixtures/helloworld.txt',
-        cwd: 'test/',
-        base: 'test/fixtures',
-        contents: fs.readFileSync('test/fixtures/helloworld.txt')
-      });
-
-      var stream = replacePlugin('world', 'person');
-      stream.on('data', function(newFile) {
-        should.exist(newFile);
-        should.exist(newFile.contents);
-
-        String(newFile.contents).should.equal(fs.readFileSync('test/expected/helloworld.txt', 'utf8'));
-        done();
       });
 
       stream.write(file);
@@ -75,26 +176,6 @@ describe('gulp-replace', function() {
       stream.end();
     });
 
-    it('should replace regex on a buffer', function(done) {
-      var file = new File({
-        path: 'test/fixtures/helloworld.txt',
-        cwd: 'test/',
-        base: 'test/fixtures',
-        contents: fs.readFileSync('test/fixtures/helloworld.txt')
-      });
-
-      var stream = replacePlugin(/world/g, 'person');
-      stream.on('data', function(newFile) {
-        should.exist(newFile);
-        should.exist(newFile.contents);
-
-        String(newFile.contents).should.equal(fs.readFileSync('test/expected/helloworld.txt', 'utf8'));
-        done();
-      });
-
-      stream.write(file);
-      stream.end();
-    });
 
     it('should replace regex on a stream with a function', function(done) {
       var file = new File({
@@ -119,26 +200,7 @@ describe('gulp-replace', function() {
       stream.end();
     });
 
-    it('should replace regex on a buffer with a function', function(done) {
-      var file = new File({
-        path: 'test/fixtures/helloworld.txt',
-        cwd: 'test/',
-        base: 'test/fixtures',
-        contents: fs.readFileSync('test/fixtures/helloworld.txt')
-      });
 
-      var stream = replacePlugin(/world/g, function() { return 'person'; });
-      stream.on('data', function(newFile) {
-        should.exist(newFile);
-        should.exist(newFile.contents);
-
-        String(newFile.contents).should.equal(fs.readFileSync('test/expected/helloworld.txt', 'utf8'));
-        done();
-      });
-
-      stream.write(file);
-      stream.end();
-    });
 
     it('should replace string on a stream with a function', function(done) {
       var file = new File({
@@ -163,26 +225,7 @@ describe('gulp-replace', function() {
       stream.end();
     });
 
-    it('should replace string on a buffer with a function', function(done) {
-      var file = new File({
-        path: 'test/fixtures/helloworld.txt',
-        cwd: 'test/',
-        base: 'test/fixtures',
-        contents: fs.readFileSync('test/fixtures/helloworld.txt')
-      });
 
-      var stream = replacePlugin('world', function() { return 'person'; });
-      stream.on('data', function(newFile) {
-        should.exist(newFile);
-        should.exist(newFile.contents);
-
-        String(newFile.contents).should.equal(fs.readFileSync('test/expected/helloworld.txt', 'utf8'));
-        done();
-      });
-
-      stream.write(file);
-      stream.end();
-    });
 
     it('should call function once for each replacement when replacing a string on a stream', function(done) {
       var file = new File({
@@ -242,59 +285,9 @@ describe('gulp-replace', function() {
       stream.end();
     });
 
-    it('should call function once for each replacement when replacing a string on a buffer', function(done) {
-      var file = new File({
-        path: 'test/fixtures/helloworld.txt',
-        cwd: 'test/',
-        base: 'test/fixtures',
-        contents: fs.readFileSync('test/fixtures/helloworld.txt')
-      });
 
-      var replacements = [
-        'cow',
-        'chicken',
-        'duck',
-        'person'
-      ];
-      var stream = replacePlugin('world', function() { return replacements.shift(); });
-      stream.on('data', function(newFile) {
-        should.exist(newFile);
-        should.exist(newFile.contents);
 
-        String(newFile.contents).should.equal(fs.readFileSync('test/expected/hellofarm.txt', 'utf8'));
-        done();
-      });
 
-      stream.write(file);
-      stream.end();
-    });
-
-    it('should call function once for each replacement when replacing a regex on a buffer', function(done) {
-      var file = new File({
-        path: 'test/fixtures/helloworld.txt',
-        cwd: 'test/',
-        base: 'test/fixtures',
-        contents: fs.readFileSync('test/fixtures/helloworld.txt')
-      });
-
-      var replacements = [
-        'cow',
-        'chicken',
-        'duck',
-        'person'
-      ];
-      var stream = replacePlugin(/world/g, function() { return replacements.shift(); });
-      stream.on('data', function(newFile) {
-        should.exist(newFile);
-        should.exist(newFile.contents);
-
-        String(newFile.contents).should.equal(fs.readFileSync('test/expected/hellofarm.txt', 'utf8'));
-        done();
-      });
-
-      stream.write(file);
-      stream.end();
-    });
 
     it('should ignore binary files when skipBinary is enabled', function(done) {
       var file = new File({
@@ -340,22 +333,6 @@ describe('gulp-replace', function() {
       stream.end();
     });
 
-    it('should trigger events on a buffer', function(done) {
-      var file = new File({
-        path: 'test/fixtures/helloworld.txt',
-        cwd: 'test/',
-        base: 'test/fixtures',
-        contents: fs.readFileSync('test/fixtures/helloworld.txt')
-      });
 
-      var stream = replacePlugin('world', 'elephant')
-      .on('finish', function() {
-        // No assertion required, we should end up here, if we don't the test will time out
-        done();
-      });
-
-      stream.write(file);
-      stream.end();
-    });
   });
 });
