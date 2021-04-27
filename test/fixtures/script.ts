7015beab381824c2017e7e14787f8d106915fdb9
Manuel Thalmann <m@nuth.ch>
2021-04-28 01:18:05 +0200

    Add tests for the new type-declarations

--- test/fixtures/script.ts
+++ test/fixtures/script.ts
@@ -5,3 +5,12 @@ replacePlugin('hello', 'world');
 
 replacePlugin(/.*/, () => 'test');
 replacePlugin('', (match, ...args) => match + '-test' + args[0]);
+
+replacePlugin(
+    '',
+    function (match)
+    {
+        console.log(match);
+        console.log(this.file.basename);
+        return '';
+    })
